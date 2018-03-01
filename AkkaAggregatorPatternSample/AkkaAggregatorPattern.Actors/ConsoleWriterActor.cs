7b245e4188a668d75eb4463662af472cdd43523b
unknown <saraniyaashrii1992@gmail.com>
2018-03-02 04:04:12 +0530

    Created Actor structure

---
+++ AkkaAggregatorPatternSample/AkkaAggregatorPattern.Actors/ConsoleWriterActor.cs
@@ -0,0 +1,51 @@
+﻿using Akka.Actor;
+using AkkaAggregatorPattern.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AkkaAggregatorPattern.Actors
+{
+    public class ConsoleWriterActor : ReceiveActor
+    {
+        public ConsoleWriterActor()
+        {
+            ReceiveAny(msg => WriteToConsole(msg));
+        }
+
+        private void WriteToConsole<T>(T msg)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Attribution data processed for fund {0}", (msg as FundData).Id);
+            Console.ResetColor();
+        }
+    }
+
+    public class TextFileWriterActor : ReceiveActor
+    {
+        public TextFileWriterActor()
+        {
+            ReceiveAny(msg => WriteToFile(msg));
+        }
+
+        private void WriteToFile<T>(T msg)
+        {
+            string filePath = "B:\\Prototypes\\Akka.net\\Akka-samples\\SampleFiles";
+            if (msg is FundData)
+            {
+                var data = msg as FundData;
+                filePath = string.Format("{0}\\Fund_{1}.txt", filePath, data.Id);
+            }
+            else
+            {
+                filePath = string.Format("{0}\\Sample.txt", filePath);
+            }
+
+            File.WriteAllText(filePath, msg.ToString());
+        }
+    }
+
+}
